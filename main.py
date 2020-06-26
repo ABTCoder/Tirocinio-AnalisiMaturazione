e4e5aa0b1dc623c2c4a5f246109247e90fb16ade
ABTCoder <amalbenson98@gmail.com>
2020-06-26 16:22:53 +0200

    Test delle tre librerie

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import cv2 as cv
 
-print(np.version)
+print(cv.__version__)
+matrix = np.arange(21).reshape(3, 7)
+print(matrix)
+fig, axis = plt.subplots()
+axis.plot([1,2,3,4], [4,7,8,4])
+plt.show()
