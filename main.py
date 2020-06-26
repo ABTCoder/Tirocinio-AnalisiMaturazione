9a77ffd89de2b44ff21625d18b79f245480ccc6d
ABTCoder <amalbenson98@gmail.com>
2020-06-26 19:09:11 +0200

    Raggruppamento codice estrazione frutti in una funzione e test di visualizzazione

--- main.py
+++ main.py
@@ -15,9 +15,12 @@ import cv2 as cv
 # axis.plot([1,2,3,4], [4,7,8,4])
 # plt.show()
 
-def readBBoxes(filename):
+# LETTURA DEI BOUNDING BOX
+def read_boxes(filename):
     bb_list = np.loadtxt(filename, int)
     converted_list = []
+    # CONVERSIONE DAL FORMATO XCENTER, YCENTER, WIDTH , HEIGHT
+    # A XSTART , XSTOP, YSTART, YSTOP
     for box in bb_list:
         half_width = math.floor(box[2]/2)
         half_height = math.floor(box[3]/2)
@@ -29,15 +32,18 @@ def readBBoxes(filename):
     return converted_list
 
 
-boxes = readBBoxes("bboxes.txt")
-img = cv.imread("olive.jpg", cv.IMREAD_UNCHANGED)
-for box in boxes:
-    subimg = img[box[2]:box[3], box[0]:box[1]]
-    subimg[subimg<100] += 45
-    img[box[2]:box[3], box[0]:box[1]] = subimg
+# ESTRAI I SINGOLI FRUTTI DALL'IMMAGINE IN BASE ALLE BOUNDING BOXES
+def extract_images(filename):
+    images = []
+    boxes = read_boxes("bboxes.txt")
+    img = cv.imread(filename, cv.IMREAD_UNCHANGED)
+    for box in boxes:
+        subimg = img[box[2]:box[3], box[0]:box[1]]
+        images.append(subimg)
+    return images
 
 
-cv.imshow("PROVA", img)
+cv.imshow("PROVA", extract_images("olive.jpg")[0])
 cv.waitKey(0)
 cv.destroyAllWindows()
 
