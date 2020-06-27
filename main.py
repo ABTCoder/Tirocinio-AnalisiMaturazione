43a4ca36cd2a14bcfbf08d78362dcef762bf3c04
ABTCoder <amalbenson98@gmail.com>
2020-06-27 17:05:59 +0200

    Test rilevamento cerchi di Hough

--- main.py
+++ main.py
@@ -4,11 +4,6 @@ import numpy as np
 import cv2 as cv
 
 
-# img = cv.imread("mela.jpg", cv.IMREAD_UNCHANGED)
-# cv.imshow("PROVA", img)
-# cv.waitKey(0)
-# cv.destroyAllWindows()
-
 # LETTURA DEI BOUNDING BOX
 def read_boxes(filename):
     bb_list = np.loadtxt(filename, int)
@@ -37,10 +32,29 @@ def extract_hsv_images(filename):
     return images
 
 
-test = extract_hsv_images("olive.jpg")[0]
+# TEST RILEVAMENTO DEI CERCHI
+def detect_circle(image):
+    height, width = image.shape[0:2]
+    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    circles = cv.HoughCircles(gray, cv.HOUGH_GRADIENT, 1, max(height, width),
+                               param1=50, param2=30, minRadius=0, maxRadius=math.floor(max(height, width)/2))
+
+    circles = np.uint16(np.around(circles))
+    for i in circles[0, :]:
+        # draw the outer circle
+        cv.circle(image, (i[0], i[1]), i[2], (0, 255, 0), 2)
+        # draw the center of the circle
+        cv.circle(image, (i[0], i[1]), 2, (0, 0, 255), 3)
+    return image
+
+
+test = extract_hsv_images("olive.jpg")[1]
+test = detect_circle(test)
 (n, bins) = np.histogram(test, bins=80, density=True)  # NumPy version (no plot)
 plt.plot(.5*(bins[1:]+bins[:-1]), n)
 plt.show()
+
+
 cv.imshow("PROVA", test)
 cv.waitKey(0)
 cv.destroyAllWindows()
