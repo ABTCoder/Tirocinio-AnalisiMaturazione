f1f3a4f00347cb2ba4d81720b83acd118ba4ba88
ABTCoder <amalbenson98@gmail.com>
2020-07-03 21:02:24 +0200

    test rilevamento ellissi

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2 as cv
+from skimage.transform import hough_ellipse
+from skimage.draw import ellipse as fill_ellipse
+from skimage.feature import canny
+from skimage.util import img_as_float, img_as_ubyte
+
 
 
 # LETTURA DEI BOUNDING BOX
@@ -23,45 +28,49 @@ def read_boxes(filename):
 
 # ESTRAI I SINGOLI FRUTTI DALL'IMMAGINE IN BASE ALLE BOUNDING BOXES
 def extract_hsv_images(filename):
-    images = []
+    hsv_images = []
+    bgr_images = []
+    masks = []
     boxes = read_boxes("bboxes.txt")
-    img = cv.imread(filename, cv.IMREAD_UNCHANGED)
+    img = cv.imread(filename)
     for box in boxes:
         subimg = img[box[2]:box[3], box[0]:box[1]]
-        subimg = detect_circle(subimg)
-        images.append(cv.cvtColor(subimg, cv.COLOR_BGR2HSV))
-    return images
+        mask = detect_ellipse(subimg)
+        bgr_images.append(subimg)
+        hsv_images.append(cv.cvtColor(subimg, cv.COLOR_BGR2HSV_FULL))
+        masks.append(mask)
+    return hsv_images, bgr_images, masks
 
 
 # TEST RILEVAMENTO DEI CERCHI
-def detect_circle(image):
-    height, width = image.shape[0:2]
+def detect_ellipse(image):
     gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    circles = cv.HoughCircles(gray, cv.HOUGH_GRADIENT, 1, max(height, width),
-                              param1=50, param2=30, minRadius=0, maxRadius=math.floor(max(height, width) * 1.2 / 2))
-
-    circles = np.uint16(np.around(circles))
-    for i in circles[0, :]:
-        # draw the outer circle
-        cv.circle(image, (i[0], i[1]), i[2], (0, 255, 0), 2)
-        # draw the center of the circle
-        cv.circle(image, (i[0], i[1]), 2, (0, 0, 255), 3)
-    return image
+    gray = canny(gray, sigma=4.0, low_threshold=0.3, high_threshold=0.8)
+    height, width = image.shape[0:2]
+    mask = np.zeros((height, width), np.uint8)
+    ellipse = hough_ellipse(gray, threshold=100, min_size=math.floor(max(height, width)/2))
+    params = ellipse.tolist()
+    rr, cc = fill_ellipse(params[1], params[2], params[3], params[4], mask.shape)
+    mask[rr, cc] = 1
+    return mask
 
 
 # CALCOLA E STAMPA L'ISTOGRAMMA
 def plot_histogram(axis, mat, label=""):
-    (n, bins) = np.histogram(mat, bins=80, density=True)  # NumPy version (no plot)
-    axis.plot(.5 * (bins[1:] + bins[:-1]), n, label=label)
+    hist = cv.calcHist([mat], [0], None, [256], [0, 256]) #Istogramma di opencv è 10x più veloce di numpy
+    axis.hist(mat.ravel(), bins=256, range=[0, 256], label=label)
+    return hist
 
 
-result = extract_hsv_images("olive.jpg")
-test = result[1]
+hsv, bgr, masks = extract_hsv_images("olive.jpg")
+test = hsv[1]
+img = cv.imread("olive.jpg")
+b, g, r = cv.split(img)
 fig1, ax1 = plt.subplots()
-plot_histogram(ax1, test)
+vals = plot_histogram(ax1, r)
 plt.show()
 
-
+# restituire anche gli istogrammi r g b
 h, s, v = cv.split(test)
 fig2, ax2 = plt.subplots()
 plot_histogram(ax2, h, "HUE")
@@ -72,8 +81,9 @@ plt.show()
 
 
 # test = test[:, :, 0]
-for im in result:
+for im in hsv:
     cv.imshow("PROVA", im)
     cv.waitKey(0)
-
+cv.imshow("PROVA", masks[3])
+cv.waitKey(0)
 cv.destroyAllWindows()
