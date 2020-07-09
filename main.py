b0168b077f03b103cd3fe56746490c730cdc343c
ABTCoder <amalbenson98@gmail.com>
2020-07-09 21:54:18 +0200

    Inserito json parser e convertito a Python 64 bit

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import math
+import json
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2 as cv
 from skimage.transform import hough_ellipse
-from skimage.draw import ellipse as fill_ellipse
+from skimage.draw import ellipse as fill_ellipse, ellipse_perimeter
 from skimage.util import img_as_float, img_as_ubyte
 
 
@@ -24,29 +25,50 @@ def read_boxes(filename):
     return converted_list
 
 
+# LETTURA BOUNDING BOXES DAL JSON
+def read_json(n):
+    b_list = []
+    with open('info.json') as f:
+        data = json.load(f)
+
+    for points in data[n]["Label"]["Olive"]:
+        xstart = points["geometry"][0]["x"]
+        xstop = points["geometry"][1]["x"]
+        ystart = points["geometry"][0]["y"]
+        ystop = points["geometry"][2]["y"]
+        b_list.append([xstart, xstop, ystart, ystop])
+
+    return b_list
+
+
 # ESTRAI I SINGOLI FRUTTI DALL'IMMAGINE IN BASE ALLE BOUNDING BOXES
 # RITORNA DUE LISTE CONTENENTI LE FREQUENZE D'ISTOGRAMMA PER OGNI CANALE
 def extract_histograms(filename, boxes_name):
     hsv_hists = []
     bgr_hists = []
-    boxes = read_boxes(boxes_name)
+    # boxes = read_boxes(boxes_name)
+    boxes = read_json(boxes_name)
     fruits = cv.imread(filename)
     for box in boxes:
-        sub_img_bgr = fruits[box[2]:box[3], box[0]:box[1]]
-        sub_img_hsv = cv.cvtColor(sub_img_bgr, cv.COLOR_BGR2HSV_FULL)
+        try:
+            sub_img_bgr = fruits[box[2]:box[3], box[0]:box[1]]
+            sub_img_hsv = cv.cvtColor(sub_img_bgr, cv.COLOR_BGR2HSV_FULL)
 
-        mask = detect_ellipse(sub_img_bgr)  # METODO 1 (ELLISSI)
-        # mask = detect_contours(sub_img_bgr)  # METODO 2
+            mask = detect_ellipse(sub_img_bgr)  # METODO 1 (ELLISSI)
+            # mask = detect_contours(sub_img_bgr)  # METODO 2
 
-        fig, ax = plt.subplots()
+            fig, ax = plt.subplots()
 
-        bh, gh, rh = plot_histogram(ax, sub_img_bgr, "BGR", mask)
-        hh, sh, vh = plot_histogram(ax, sub_img_hsv, "HSV", mask)
-        ax.legend()
-        plt.show()
+            bh, gh, rh = plot_histogram(ax, sub_img_bgr, "BGR", mask)
+            hh, sh, vh = plot_histogram(ax, sub_img_hsv, "HSV", mask)
+            ax.legend()
+            plt.show()
 
-        bgr_hists.append([bh, gh, rh])
-        hsv_hists.append([hh, sh, vh])
+            bgr_hists.append([bh, gh, rh])
+            hsv_hists.append([hh, sh, vh])
+        except Exception:
+            continue
+
     return hsv_hists, bgr_hists
 
 
@@ -54,16 +76,18 @@ def extract_histograms(filename, boxes_name):
 # RESTITUISCE LA MASCHERA , None SE NON E' STATA GENERATA
 def detect_ellipse(image):
     gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)  # CONVERSIONE IN SCALA DI GRIGI
+
     # gray = cv.Laplacian(gray, cv.CV_16S, ksize=3)
     # gray = cv.convertScaleAbs(gray)
 
     gray = cv.bilateralFilter(gray, 9, 75, 75)  # SFOCATURA
     # gray = cv.GaussianBlur(gray, (5, 5), 0)
-    # gray = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2)
+    # gray = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY_INV, 9, 7)
+    # gray = cv.bitwise_not(gray)
     cv.imshow("BLUR", gray)
     cv.waitKey(0)
 
-    sigma = 0.33
+    sigma = 0.6
     v = np.median(image)
     # apply automatic Canny edge detection using the computed median
     lower = int(max(0, (1.0 - sigma) * v))
@@ -74,9 +98,9 @@ def detect_ellipse(image):
     cv.waitKey(0)
     cv.destroyAllWindows()
 
-    height, width = image.shape[0:2]
+    height, width = gray.shape[0:2]
     mask = np.zeros((height, width), np.uint8)
-    ellipses = hough_ellipse(gray, threshold=200, accuracy=100, min_size=round(max(height, width)/2),
+    ellipses = hough_ellipse(gray, threshold=8, accuracy=100, min_size=round(max(height, width)/2),
                              max_size=round(min(height, width)))  # RICERCA ELLISSI
     if ellipses.size > 0:
         # ESTRAI L'ELLISSE MIGLIORE
@@ -106,9 +130,13 @@ def detect_contours(image):
     # gray = cv.Laplacian(gray, cv.CV_16S, ksize=3)
     # gray = cv.convertScaleAbs(gray)
 
-    gray = cv.bilateralFilter(gray, 9, 75, 75)  # SFOCATURA
+    # gray = cv.bilateralFilter(gray, 9, 75, 75)  # SFOCATURA
     # gray = cv.GaussianBlur(gray, (5, 5), 0)
-    # gray = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2)
+    # gray = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY_INV, 9, 7)
+    _, gray = cv.threshold(gray, 150, 255, cv.THRESH_BINARY_INV)
+    kernel = cv.getStructuringElement(cv.MORPH_CROSS, (3, 3))
+    # gray = cv.dilate(gray, kernel, iterations=2)  # dilate
+    # gray = cv.bitwise_not(gray)
     cv.imshow("BLUR", gray)
     cv.waitKey(0)
 
@@ -117,17 +145,20 @@ def detect_contours(image):
     # apply automatic Canny edge detection using the computed median
     lower = int(max(0, (1.0 - sigma) * v))
     upper = int(min(255, (1.0 + sigma) * v))
-    gray = cv.Canny(gray, lower, upper)
+    # gray = cv.Canny(gray, lower, upper)
     cv.imshow("CANNY", gray)
     cv.waitKey(0)
 
     se = np.ones((7, 7), dtype='uint8')
-    gray = cv.morphologyEx(gray, cv.MORPH_CLOSE, se, iterations=2)  # CHIUDE I BUCHI DEI CONTORNI
+    # gray = cv.morphologyEx(gray, cv.MORPH_CLOSE, se, iterations=1)  # CHIUDE I BUCHI DEI CONTORNI
     cv.imshow("MORP", gray)
     cv.waitKey(0)
     contours, hierarchy = cv.findContours(gray, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-    # mask = cv.drawContours(gray, contours, -1, 255, 3)
-    mask = cv.fillPoly(gray, contours, 255)
+
+    height, width = gray.shape[0:2]
+    mask = np.zeros((height, width), np.uint8)
+    # mask = cv.drawContours(gray, hull_list, -1, 255, 3)
+    mask = cv.fillPoly(mask, contours, 255)
     cv.imshow("FILL", mask)
     cv.waitKey(0)
     cv.destroyAllWindows()
@@ -155,7 +186,8 @@ def plot_histogram(axis, image, label="123", mask=None):
     return r1, r2, r3
 
 
-hsv, bgr = extract_histograms("mele.jpg", "bboxes_mele.txt")
+read_json(0)
+hsv, bgr = extract_histograms("images/1.jpg", 0)
 
 # img = cv.imread("olive2.jpg")
 # fig1, ax1 = plt.subplots()
